1433406c5927c4ff86c14932239b65be9d8c0ebe
Dmitri Voitsekhovitch <dvoitsekh@gmail.com>
2015-03-20 17:17:26 +0100

    Fixed css and opacity effects.

--- src/menu/Link.js
+++ src/menu/Link.js
@@ -11,6 +11,22 @@ const grey3 = rsg.colours.grisclair2;
 //
 require('../styles/Link.css');
 
+var linkStyle = {
+    fontFamily: '"Trebuchet MS", sans-serif',
+    padding: '2px 4px 2px 4px',
+    width: '150px',
+    textDecoration: 'none',
+    whiteSpace: 'normal',
+    verticalAlign: 'middle',
+    lineHeight: '30px', /* Set text height here for v-align */
+    display: 'block',
+    fontSize: '17px',
+    border: 'none',
+    backgroundColor: grey3,
+    height: '30px',
+    color: grey1
+};
+
 //
 // CODE
 //
@@ -18,8 +34,8 @@ require('../styles/Link.css');
 var Button = React.createClass({
     render: function() {
         return (
-            <span>
-                <a href={this.props.url} className='recolnatUpperNavigationBarButton'>{this.props.label}</a>
+            <span className='recolnatUpperNavigationBarButtonContainer'>
+                <a style={linkStyle} href={this.props.url} >{this.props.label}</a>
             </span>
         );
     }
--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -3,6 +3,13 @@
 import React from 'react';
 import Button from './Link';
 import rsg from 'recolnat-style-guide';
+const grey1 = rsg.colours.grisfonce;
+const grey2 = rsg.colours.grisclair1;
+const grey3 = rsg.colours.grisclair2;
+//
+// GLOBAL
+//
+var menuBarHeight = '35px';
 
 //
 // STYLES
@@ -11,24 +18,52 @@ import rsg from 'recolnat-style-guide';
 import '../../node_modules/normalize.css/normalize.css';
 require('../styles/Menu.css');
 
+var recolnatUpperNavigationBarLinkListStyle = {
+    display: 'flex',
+    padding: '0px',
+    margin: '0px',
+    justifyContent: 'center'
+};
+
+var recolnatUpperNavigationBarLinkListItemStyle = {
+    width: '150px',
+    height: menuBarHeight,
+    display: 'inline',
+    textAlign: 'center',
+    overflow: 'hidden',
+    paddingLeft: '10px'
+};
+
+var recolnatUpperNavigationBarNavStyle = {
+    position: 'fixed',
+    top: '0px',
+    backgroundColor: grey3,
+    fontFamily: '"Trebuchet MS", sans-serif',
+    width: '100%',
+    padding: '0px',
+    boxShadow: '5px 5px 5px ' + grey3
+};
+
 //
 // COMP
 //
 
-import recolnatLogoUrl from '../../node_modules/recolnat-style-guide/images/recolnat_W_H40.png';
+import recolnatLogoUrl from '../../node_modules/recolnat-style-guide/images/recolnat_B_H40.png';
 
 var Comp = React.createClass({
   render: function() {
     return (
-        <nav className='recolnatUpperNavigationBarNav'>
-          <ul className='recolnatUpperNavigationBarLinkList'>
-            <li className='recolnatUpperNavigationBarLinkListItem' ><img src={recolnatLogoUrl} height={this.props.menuHeight} /></li>
+        <nav className='recolnatUpperNavigationBarNav' >
+            <span style={recolnatUpperNavigationBarNavStyle}>
+                <ul style={recolnatUpperNavigationBarLinkListStyle}>
+                    <li style={recolnatUpperNavigationBarLinkListItemStyle} ><img src={recolnatLogoUrl} height={this.props.menuHeight} /></li>
         {this.props.recolnatModules.map(function(rm) {
-          return <li key={rm.url} className='recolnatUpperNavigationBarLinkListItem'>
-            <Button url={rm.url} label={rm.label} />
-          </li>;
+            return <li key={rm.url} style={recolnatUpperNavigationBarLinkListItemStyle}>
+                <Button url={rm.url} label={rm.label} />
+            </li>;
         })}
-          </ul>
+                </ul>
+            </span>
         </nav>
     );
   }
